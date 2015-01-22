6c47a9be30dde13d69590bd722c7821cfef74af2
sam2k13 <sam2k13@gmail.com>
2015-01-22 00:11:13 -0600

    Added Leaderboard

--- Shape Shifters/Assets/Scripts/ChooseDifficulty.cs
+++ Shape Shifters/Assets/Scripts/ChooseDifficulty.cs
@@ -3,10 +3,18 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class ChooseDifficulty : MonoBehaviour {
-	
+	void Start(){
+		GooglePlayController.googlePlay.SignIn ();
+
+	}
 	public void  loadFunction()
 	{
 		Application.LoadLevel("Choose Difficulty");
+	}
+	public void showLeaderboard(){
+		GooglePlayController.googlePlay.SignIn ();
+		GooglePlayController.googlePlay.UpdateLeaderboard (GameData.control.highScore);
+		GooglePlayController.googlePlay.ShowLeaderboard ();
 	}
 }
 
