cf97cf87aa80877460a942ad6e0c0dfe27f5ba1f
kapra004 <kkapraun3221@aol.com>
2015-02-05 19:55:30 -0600

    All Menus Changed

---
+++ Shape Shifters/Assets/Scripts/LoadHome.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadHome : MonoBehaviour {
+
+	public void  loadHome()
+	{
+		Application.LoadLevel("Title Page");
+	}
+}
