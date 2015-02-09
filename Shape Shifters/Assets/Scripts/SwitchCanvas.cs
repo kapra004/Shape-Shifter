04acb00b87250b4c06aaa8a12ea0bba20b6f54b9
kapra004 <kkapraun3221@aol.com>
2015-02-09 07:04:03 -0600

    New APK

---
+++ Shape Shifters/Assets/Scripts/SwitchCanvas.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SwitchCanvas : MonoBehaviour {
+
+	public GameObject mainCanvas;
+	public GameObject helpCanvas;
+	public GameObject pSystem1;
+	public GameObject pSystem2;
+	public GameObject pSystem3;
+
+	public void disableCanvas()
+	{
+		mainCanvas.SetActive (false);
+		pSystem1.SetActive (false);
+		pSystem2.SetActive (false);
+		pSystem3.SetActive (false);
+		helpCanvas.SetActive (true);
+
+	}
+
+	public void enableCanvas()
+	{
+		helpCanvas.SetActive (false);
+		mainCanvas.SetActive (true);
+		pSystem1.SetActive (true);
+		pSystem2.SetActive (true);
+		pSystem3.SetActive (true);
+	}
+
+}
