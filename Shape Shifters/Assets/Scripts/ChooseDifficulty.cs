aefea2f5d6121674d0964df00c736ac839afdef6
kapra004 <kkapraun3221@aol.com>
2015-04-06 12:40:47 -0500

    Deleted Leader Board

--- Shape Shifters/Assets/Scripts/ChooseDifficulty.cs
+++ Shape Shifters/Assets/Scripts/ChooseDifficulty.cs
@@ -5,17 +5,12 @@ using UnityEngine.UI;
 public class ChooseDifficulty : MonoBehaviour {
 	void Start()
 	{
-		GooglePlayController.googlePlay.SignIn ();
+
 	}
 	public void  chooseDifficulty()
 	{
 		Application.LoadLevel("Choose Difficulty");
 	}
-	public void showLeaderboard()
-	{
-		GooglePlayController.googlePlay.SignIn ();
-		GooglePlayController.googlePlay.UpdateLeaderboard (GameData.control.highScore);
-		GooglePlayController.googlePlay.ShowLeaderboard ();
-	}
+
 }
 
